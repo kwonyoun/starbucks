905fab2d89916b5e474543f8ed939fb09808ab2c
kwon <kyjyj6334@gmail.com>
2025-12-09 17:43:29 +0900

    feat: Add order

---
+++ src/main/java/com/example/starbucks/dao/OrderDao.java
@@ -0,0 +1,8 @@
+package com.example.starbucks.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderDao {
+    
+}
---
+++ src/main/java/com/example/starbucks/service/OrderService.java
@@ -0,0 +1,8 @@
+package com.example.starbucks.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    
+}
